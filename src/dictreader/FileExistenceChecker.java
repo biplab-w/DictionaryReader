748f76c8cab23a20a11b8408978bc510c9661c6c
biplab-w <waglebip@gmail.com>
2023-06-18 00:00:29 -0500

    Dictionary reader all --first uplink

---
+++ src/dictreader/FileExistenceChecker.java
@@ -0,0 +1,10 @@
+package dictreader;
+
+import java.io.File;
+
+public class FileExistenceChecker {
+    public static boolean doesFileExist(String filePath) {
+        File file = new File(filePath);
+        return file.exists();
+    }
+}
